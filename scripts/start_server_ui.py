04ed42271d01c4e24e7c8e0bf771ea2c687861a9
Evgeniy Gleba <60469435+geugenm@users.noreply.github.com>
2023-12-02 19:24:46 +0300

    Refactored script. Logs handling and status displaying

--- scripts/start_server_ui.py
+++ scripts/start_server_ui.py
@@ -7,15 +7,18 @@ import argparse
 import os
 import http.client
 import logging
-
+from datetime import datetime
+from tkinter import messagebox
 
 class ServerUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Python Server")
 
-        self.directory = tk.StringVar(value="./")  # Default directory
+        self.directory = tk.StringVar(value="")  # Default directory
         self.port = tk.StringVar(value="8080")  # Default port
+        self.status_label = tk.Label(self.root, text="STATUS: OFF", fg="red")
+        self.status_label.pack()
 
         self.start_button = tk.Button(self.root, text="Start", command=self.start_server)
         self.start_button.pack()
@@ -25,9 +28,6 @@ class ServerUI:
 
         self.open_button = tk.Button(self.root, text="Open URL", command=self.open_url, state=tk.DISABLED)
         self.open_button.pack()
-
-        self.check_button = tk.Button(self.root, text="Check", command=self.check_server, state=tk.DISABLED)
-        self.check_button.pack()
 
         tk.Label(self.root, text="Directory to host:").pack()
         self.directory_entry = tk.Entry(self.root, textvariable=self.directory)
@@ -46,25 +46,32 @@ class ServerUI:
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
         # Log to file
-        self.log_file = "log.txt"  # Default log file
+        log_directory = "logs"
+        if not os.path.exists(log_directory):
+            os.makedirs(log_directory)
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.log_file = os.path.join(log_directory, f"log_{timestamp}.txt")
         file_handler = logging.FileHandler(self.log_file)
         file_handler.setFormatter(formatter)
         self.logger.addHandler(file_handler)
 
-        # Log to console
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(formatter)
-        self.logger.addHandler(console_handler)
+        self.logger.info(f"Server started at {timestamp}")
 
     def start_server(self):
         self.start_button.config(state=tk.DISABLED)
         self.directory_entry.config(state=tk.DISABLED)
         self.port_entry.config(state=tk.DISABLED)
 
-        directory = os.path.abspath(self.directory.get())
-        port = int(self.port.get())
+        directory = self.directory.get()
+        if not os.path.exists(directory):
+            self.logger.info(f"Directory '{directory}' does not exist.")
+            messagebox.showinfo("Error", f"Directory '{directory}' does not exist.")
+            self.start_button.config(state=tk.NORMAL)
+            self.directory_entry.config(state=tk.NORMAL)
+            self.port_entry.config(state=tk.NORMAL)
+            return
 
-        os.chdir(directory)
+        port = int(self.port.get())
 
         self.server = socketserver.TCPServer(("localhost", port), http.server.SimpleHTTPRequestHandler)
         self.server_thread = threading.Thread(target=self.server.serve_forever)
@@ -73,8 +80,7 @@ class ServerUI:
 
         self.stop_button.config(state=tk.NORMAL)
         self.open_button.config(state=tk.NORMAL)
-        self.check_button.config(state=tk.NORMAL)
-
+        self.status_label.config(text="STATUS: ON", fg="green")
         self.logger.info(f"Server started at http://localhost:{port} serving directory {directory}")
 
     def stop_server(self):
@@ -83,7 +89,7 @@ class ServerUI:
             self.server.server_close()
             self.stop_button.config(state=tk.DISABLED)
             self.open_button.config(state=tk.DISABLED)
-            self.check_button.config(state=tk.DISABLED)
+            self.status_label.config(text="STATUS: OFF", fg="red")
             self.directory_entry.config(state=tk.NORMAL)
             self.port_entry.config(state=tk.NORMAL)
             self.start_button.config(state=tk.NORMAL)
@@ -92,17 +98,6 @@ class ServerUI:
     def open_url(self):
         port = int(self.port.get())
         webbrowser.open_new_tab(f"http://localhost:{port}")
-
-    def check_server(self):
-        port = int(self.port.get())
-        try:
-            conn = http.client.HTTPConnection("localhost", port)
-            conn.request("HEAD", "/")
-            res = conn.getresponse()
-            if res.status == 200:
-                self.logger.info(f"Server is running at http://localhost:{port}")
-        except ConnectionRefusedError:
-            self.logger.info(f"Server is not running at http://localhost:{port}")
 
 
 def run_server(directory, port, log_file):
@@ -116,7 +111,7 @@ def run_server(directory, port, log_file):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Python Server with UI")
-    parser.add_argument("-dir", "--directory", type=str, default="./", help="Directory to host")
+    parser.add_argument("-dir", "--directory", type=str, default="", help="Directory to host")
     parser.add_argument("-p", "--port", type=int, default=8080, help="Port to run the server on")
     parser.add_argument("-log", "--logfile", type=str, default="log.txt", help="Log file name")
     args = parser.parse_args()
