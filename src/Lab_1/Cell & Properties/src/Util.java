d9d4c2949ed3eb38fc0e4552123e166487a7b785
Evgeniy Gleba <60469435+geugenm@users.noreply.github.com>
2023-12-10 17:03:37 +0300

    Refactored Utils + Removed unused frame

--- src/Lab_1/Cell & Properties/src/Util.java
+++ src/Lab_1/Cell & Properties/src/Util.java
@@ -1,48 +1,27 @@
 import java.applet.Applet;
 import java.awt.*;
 
-
-class Util {
+public class Util {
 
     public static Frame getFrame(Component component) {
-        /* 15 */
-        Object obj = component;
-        /* 16 */
-        if (obj instanceof Frame)
-            /* 17 */ return (Frame) obj;
-        /* 18 */
-        while ((obj = ((Component) obj).getParent()) != null) {
-            /* 19 */
-            if (obj instanceof Frame)
-                /* 20 */ return (Frame) obj;
-            /* 21 */
+        Component currentComponent = component;
+        while (currentComponent != null) {
+            if (currentComponent instanceof Frame) {
+                return (Frame) currentComponent;
+            }
+            currentComponent = currentComponent.getParent();
         }
         return null;
-
     }
-
 
     public static Applet getApplet(Component component) {
-        /* 26 */
-        Object obj = component;
-        /* 27 */
-        if (obj instanceof Applet)
-            /* 28 */ return (Applet) obj;
-        /* 29 */
-        while ((obj = ((Component) obj).getParent()) != null) {
-            /* 30 */
-            if (obj instanceof Applet)
-                /* 31 */ return (Applet) obj;
-            /* 32 */
+        Component currentComponent = component;
+        while (currentComponent != null) {
+            if (currentComponent instanceof Applet) {
+                return (Applet) currentComponent;
+            }
+            currentComponent = currentComponent.getParent();
         }
         return null;
-
     }
-
 }
-
-
-/* Location:              C:\Users\Evgeniy\IdeaProjects\modern-lab-comp-applets\src\Lab_1\Cell & Properties\!\Util.class
- * Java compiler version: 1 (45.3)
- * JD-Core Version:       1.1.3
- */
