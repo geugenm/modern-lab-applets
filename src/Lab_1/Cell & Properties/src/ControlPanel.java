98a0323ef535512e3d3883cafb666e13fa1f6a9e
Evgeniy Gleba <60469435+geugenm@users.noreply.github.com>
2023-12-10 16:41:18 +0300

    Small fixes and refactorings: Refactored ControlPanel and CrystalViewer

--- src/Lab_1/Cell & Properties/src/ControlPanel.java
+++ src/Lab_1/Cell & Properties/src/ControlPanel.java
@@ -1,75 +1,75 @@
 import java.awt.*;
 
-
-class ControlPanel extends Panel {
+public class ControlPanel extends Panel {
     private final Panel displayPanel;
     DisplayArea display;
     Button showBonds;
     Label displayMesg;
     Choice models;
 
-
-    public ControlPanel(DisplayArea displayarea) {
-        this.display = displayarea;
+    public ControlPanel(DisplayArea displayArea) {
+        this.display = displayArea;
         this.models = new Choice();
-        this.models.addItem("AlAs");
-        this.models.addItem("AlP");
-        this.models.addItem("AlSb");
-        this.models.addItem("CdS");
-        this.models.addItem("CdSe");
-        this.models.addItem("CdTe");
-        this.models.addItem("GaAs");
-        this.models.addItem("GaP");
-        this.models.addItem("GaSb");
-        this.models.addItem("Ge");
-        this.models.addItem("InAs");
-        this.models.addItem("InP");
-        this.models.addItem("InSb");
-        this.models.addItem("Si");
-        this.models.addItem("ZnS");
-        this.models.addItem("ZnSe");
-        this.models.addItem("ZnTe");
-        this.showBonds = new Button("Показать границы");
-        this.displayMesg = new Label(" ", 1);
-        this.displayPanel = new Panel();
-        this.displayPanel.setLayout(new BorderLayout());
-        this.displayPanel.add("West", this.showBonds);
-        this.displayPanel.add("Center", this.displayMesg);
-        this.displayPanel.add("East", this.models);
+        addModelItems();
+        this.showBonds = new Button(getShowBondsLabel());
+        this.displayMesg = new Label(" ", Label.CENTER);
+        this.displayPanel = new Panel(new BorderLayout());
+        configureDisplayPanel();
         setLayout(new BorderLayout());
-        add("North", this.displayPanel);
+        add(this.displayPanel, BorderLayout.NORTH);
     }
 
+    private void addModelItems() {
+        final String[] modelItems = {
+                "AlAs", "AlP", "AlSb", "CdS", "CdSe", "CdTe", "GaAs", "GaP", "GaSb",
+                "Ge", "InAs", "InP", "InSb", "Si", "ZnS", "ZnSe", "ZnTe"
+        };
+        for (String item : modelItems) {
+            this.models.addItem(item);
+        }
+    }
+
+    private String getShowBondsLabel() {
+        return "Показать границы";
+    }
+
+    private String getHideBondsLabel() {
+        return "Спрятать границы";
+    }
+
+    private void configureDisplayPanel() {
+        this.displayPanel.add(this.showBonds, BorderLayout.WEST);
+        this.displayPanel.add(this.displayMesg, BorderLayout.CENTER);
+        this.displayPanel.add(this.models, BorderLayout.EAST);
+    }
 
     @Override
     public boolean action(Event event, Object obj) {
-        /* 35 */
         if (event.target == this.models) {
-            String s = obj.toString();
-            this.display.setModel(s);
+            handleModelSelection(obj);
+        } else if (event.target == this.showBonds) {
+            handleShowBonds(obj);
         }
-        /* 49 */
-        if (event.target == this.showBonds)
-            /* 50 */ if ("Показать границы".equals(obj)) {
-
-            /* 52 */
-            this.display.showBonds();
-            /* 53 */
-            this.showBonds.setLabel("Спрятать границы");
-
-        }
-        /* 55 */
-        else if ("Спрятать границы".equals(obj)) {
-
-            /* 57 */
-            this.display.hideBonds();
-            /* 58 */
-            this.showBonds.setLabel("Показать границы");
-
-        }
-        /* 60 */
         return true;
-
     }
 
+    private void handleModelSelection(Object obj) {
+        if (obj instanceof String) {
+            final String selectedModel = obj.toString();
+            this.display.setModel(selectedModel);
+        }
+    }
+
+    private void handleShowBonds(Object obj) {
+        final String showLabel = getShowBondsLabel();
+        final String hideLabel = getHideBondsLabel();
+
+        if (showLabel.equals(obj)) {
+            this.display.showBonds();
+            this.showBonds.setLabel(hideLabel);
+        } else if (hideLabel.equals(obj)) {
+            this.display.hideBonds();
+            this.showBonds.setLabel(showLabel);
+        }
+    }
 }
