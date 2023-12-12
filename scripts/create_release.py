d72525257d23e9a89f1af8e77ef454e04d4198eb
Evgeniy Gleba <60469435+geugenm@users.noreply.github.com>
2023-12-13 00:39:50 +0300

    Added create_release.py script

---
+++ scripts/create_release.py
@@ -0,0 +1,95 @@
+import logging
+import os
+import platform
+import shutil
+import subprocess
+import zipfile
+
+
+def setup_logger():
+    logging.basicConfig(filename='logs/create_release.log', level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def clean_build():
+    # Delete .build directory if it exists
+    if os.path.exists('.build'):
+        shutil.rmtree('.build', ignore_errors=True)
+
+
+def copy_files():
+    # Define source and destination directories
+    source_dirs = ['docs', 'src']
+    exclude_dirs = ['src/decompiled', 'src/logs']
+    destination_dir = '.build'
+
+    # Check if required directories exist
+    if not all(os.path.exists(dir) for dir in ['docs', 'src', 'scripts']):
+        logging.error("Required directories (docs/, src/, scripts/) not found.")
+        return
+
+    # Create destination directory if it doesn't exist
+    os.makedirs(destination_dir, exist_ok=True)
+
+    # Copy files from source to destination, excluding certain directories
+    for src_dir in source_dirs:
+        for root, dirs, files in os.walk(src_dir):
+            if any(exclude_dir in root for exclude_dir in exclude_dirs):
+                continue
+            for file in files:
+                src_file = os.path.join(root, file)
+                dest_file = os.path.join(destination_dir, os.path.relpath(src_file, start=src_dir))
+                os.makedirs(os.path.dirname(dest_file), exist_ok=True)
+                shutil.copy(src_file, dest_file)
+                logging.info(f"Copied {src_file} to {dest_file}")
+
+
+def start_compile():
+    # Change working directory to scripts/
+    os.chdir('scripts')
+
+    # Start subprocess to run compile_to_exe.py with flags
+    logging.info("Starting compile_to_exe.py...")
+    compile_process = subprocess.run(['python', 'compile_to_exe.py', 'start_server_ui.py', '--config=debug'],
+                                     capture_output=True, text=True)
+
+    if compile_process.returncode != 0:
+        logging.error("compile_to_exe.py failed. See below for details:")
+        logging.error(compile_process.stdout)
+        logging.error(compile_process.stderr)
+        return
+
+    # Move the compiled file to .build directory
+    current_os = platform.system().lower()
+    compiled_file = f".build/debug/start_server_ui.exe"  # Replace with actual extension
+    shutil.move(compiled_file, f"../.build/start_server_ui.exe")
+    logging.info(f"Moved {compiled_file} to .build folder")
+
+    # Remove unnecessary directories and files
+    shutil.rmtree('build', ignore_errors=True)
+    shutil.rmtree('dist', ignore_errors=True)
+    os.remove('start_server_ui.spec')  # Remove start_server_ui.spec file
+    logging.info("Cleaned up build artifacts")
+
+
+def compress_files():
+    # Compress .build directory contents to a zip file
+    os.chdir('..')  # Move back to the original directory
+    current_os = platform.system().lower()
+    architecture = platform.machine()
+    zip_filename = f".build/release-{current_os}-{architecture}.zip"
+    with zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk('.build'):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, os.path.relpath(file_path, '.build'))
+                logging.info(f"Added {file_path} to {zip_filename}")
+
+
+# Main execution
+if __name__ == "__main__":
+    setup_logger()
+    clean_build()
+    copy_files()
+    start_compile()
+    compress_files()
